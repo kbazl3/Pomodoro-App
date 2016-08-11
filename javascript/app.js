2c5cd10e1ef00f829bd5b2835b8cc74174a64a60
Kyle Barlow <kbazlo@yahoo.com>
2016-08-11 00:09:27 -0600

    added styling

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,1 @@
-angular.module("pomodoro", [])
-    .filter('countdown', function() {
-        return function(currentTime, endTime) {
-            if (currentTime >= endTime) {
-                alert("Pomodoro Over!")
-                return 0;
-            }
-            return endTime - currentTime;
-        };
-    });
+angular.module("pomodoro", []);
--- javascript/mainCtrl.js
+++ javascript/mainCtrl.js
@@ -1,6 +1,10 @@
 angular.module("pomodoro")
     .controller("mainCtrl", function($scope, $timeout) {
         $scope.timeRemaining = 0;
+        $scope.timerz = true;
+        $scope.stopPomodoroButton = true;
+        $scope.startPomodoroButton = false;
+
         $scope.pomodoro = function() {
             var endTime = new Date();
             endTime.setMinutes(endTime.getMinutes() + 25);
@@ -8,12 +12,33 @@ angular.module("pomodoro")
             $scope.current = {
                 endTime: endTime
             };
+            $scope.timerz = !$scope.timerz;
+            $scope.stopPomodoroButton = false;
+            $scope.startPomodoroButton = true;
+            $scope.defaultTime = true;
             $timeout($scope.updateTime, 1000);
         };
+
+        $scope.stopPomodoro = function() {
+            $scope.stopPomodoroButton = true;
+            $scope.startPomodoroButton = false;
+            $scope.defaultTime = false;
+            $scope.timerz = !$scope.timerz;
+        }
 
         $scope.updateTime = function() {
             $scope.now = Date.now();
+            console.log($scope.now);
             $timeout($scope.updateTime, 1000);
         };
 
+})
+.filter('countdown', function() {
+    return function(currentTime, endTime) {
+        if (currentTime >= endTime) {
+            alert("Pomodoro Over!")
+            return 0;
+        }
+        return endTime - currentTime;
+    };
 });
